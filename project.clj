ca51619841c1352de59f39be248325d4cfac486c
zsau <zsau@fastmail.com>
2020-07-20 14:57:00 -0700

    update deps

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject zsau/mail "0.1.0"
 	:description "Clojure wrapper for javax.mail"
 	:dependencies [
-		[org.clojure/clojure "1.9.0"]
-		[com.sun.mail/javax.mail "1.6.1"]])
+		[org.clojure/clojure "1.10.1"]
+		[com.sun.mail/javax.mail "1.6.2"]])
